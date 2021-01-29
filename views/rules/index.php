687d55ea8798f5a69fdfbe0e44aee87e9001882f
iredondoa <iredondoa@zalcu.com>
2021-01-29 16:45:06 +0100

    In rules view: first version datatables is added

--- views/rules/index.php
+++ views/rules/index.php
@@ -14,7 +14,7 @@ $user = User::get_user_from_user($_SESSION['user']);
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>User</title>
+        <title>Normas</title>
         <!-- Favicon-->
         <link rel="icon" type="image/x-icon" href="../../assets/img/favicon.ico" />
         <!-- Font Awesome icons (free version)-->
@@ -24,6 +24,17 @@ $user = User::get_user_from_user($_SESSION['user']);
         <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
         <!-- Core theme CSS (includes Bootstrap)-->
         <link href="../../css/styles.css" rel="stylesheet" />
+
+        <!-- Bootstrap core JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Third party plugin JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="../../assets/datatables/dataTables.bootstrap.min.css" />
+        <script src="../../assets/datatables/jquery.dataTables.min.js"></script>
+        <script src="../../assets/datatables/dataTables.bootstrap.min.js"></script>
     </head>
     <body id="page-top">
         <!-- Navigation-->
@@ -43,8 +54,13 @@ $user = User::get_user_from_user($_SESSION['user']);
                 <!-- Rules Section -->
                 <div class="row">
                     <div class="col-lg-8 mx-auto">
+                        <div class="table-responsive">
+                            <table id="the-table" class="table table-striped compact nowrap" style="min-width:100%">
+                                <thead><!-- Leave empty. Column titles are automatically generated --></thead>
+                            </table>
+                        </div>
                         <a href="edit_create.php">
-                            <button class="btn btn-primary btn-xl ml-1" id="create_child" type="button">Crear norma</button>
+                            <button class="btn btn-primary btn-xl ml-2" id="create_child" type="button">Crear norma</button>
                         </a>
                     </div>
                 </div>
@@ -56,14 +72,58 @@ $user = User::get_user_from_user($_SESSION['user']);
         <div class="scroll-to-top d-lg-none position-fixed mt-5">
             <a class="js-scroll-trigger d-block text-center text-white rounded" href="#page-top"><i class="fa fa-chevron-up"></i></a>
         </div>
-        <!-- Bootstrap core JS-->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
-        <!-- Third party plugin JS-->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
         <!-- Contact form JS-->
         <script src="../../assets/mail/jqBootstrapValidation.js"></script>
         <!-- Core theme JS-->
         <script src="../../js/scripts.js"></script>
+        <script type="text/javascript">
+            window.addEventListener('load', function () {
+                let table = $('#the-table').DataTable({
+                    order: [[1, 'asc']],
+                    serverSide: true,
+                    language: {
+                        url: "<?php echo APP_ROOT ?>/assets/datatables/es.json",
+                    },
+                    columns: [
+                        {
+                            sorting: false,
+                            defaultContent:
+                                '<button type="button" data-toggle="tooltip" title="Detalles" class="btn btn-success btn-sm btn-just-icon btn-link m-0"><i class="material-icons">edit</i></button>',
+                            "searchable": false,
+                        },
+                        {
+                            data: 'title',
+                            title: 'Título',
+                        },
+                        {
+                            data: 'description',
+                            title: 'Description',
+                            "searchable": false,
+                        },
+                        {
+                            data: 'consequences',
+                            title: 'Consequencias',
+                        },
+                        {
+                            data: 'img_consequences',
+                            title: 'Imagen consecuencia',
+                            "searchable": false,
+                        }
+                    ],
+                    ajax: {
+                        method: 'POST',
+                        url: "./list_rules.php",
+                        data: [],
+                        error: function(xhr) {
+                            if (xhr.status === 401) { // Session expired
+                                window.location.reload();
+                            } else {
+                                console.log(xhr);
+                            }
+                        },
+                    },
+                });
+            });
+        </script>
     </body>
 </html>

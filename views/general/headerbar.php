66c8dfb930ea0174e86ea1e00f14663f85c08c2d
iredondoa <iredondoa@zalcu.com>
2021-03-04 18:32:28 +0100

    In headerbar stop and img password child are added

--- views/general/headerbar.php
+++ views/general/headerbar.php
@@ -1,20 +1,32 @@
+<?php
+
+$user_child = User::get_user_from_user($_SESSION['user'])
+
+?>
+
 <!-- Navigation-->
 <nav class="navbar navbar-expand-lg bg-secondary fixed-top" id="mainNav">
     <div class="container">
-        <h4>
+        <h5>
             <a class="navbar-brand js-scroll-trigger" href="<?php echo APP_ROOT; ?>index.php">Disciplina positiva</a>
-        </h4>
+        </h5>
         <button class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             Menú
             <i class="fas fa-bars"></i>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
+            <?php  if ($_SESSION['type']) :  ?>
+                <a class="ml-5" href="../stop/">
+                    <img id="img-user" src="../../assets/img/stop.png" height="80" width="90"/>
+                </a>
+            <?php  else:;  ?>
+                <img id="img-user" src="../../assets/img/user_child/<?php  echo $user_child->image();  ?>" height="130" width="125"/>
+            <?php  endif;  ?>
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="../rules/">Normas</a></li>
                 <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="../meeting/">Juntas</a></li>
                 <?php  if ($_SESSION['type']){  ?>
 	                <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#turnos">Turnos</a></li>
-	                <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#stop">Stop</a></li>
                 <?php } else {  ?>
 	                <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="../wheel/">Rueda</a></li>
 	                <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#mesa">Mesa</a></li>

27dde4784da8405e2cca76d503f18c4484c08b11
iredondoa <iredondoa@zalcu.com>
2021-04-11 00:56:38 +0200

    style(footer functional)

--- views/general/footer_template.php
+++ views/general/footer_template.php
@@ -4,10 +4,9 @@
             <!-- Footer Social Icons-->
             <div class="col-lg-4 mb-5 mb-lg-0">
                 <h4 class="text-uppercase mb-4">Redes sociales</h4>
-                <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-facebook-f"></i></a>
-                <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-twitter"></i></a>
-                <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-linkedin-in"></i></a>
-                <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-dribbble"></i></a>
+                <a class="btn btn-outline-light btn-social mx-1" href="https://www.facebook.com/Disciplina-Positiva-Espa%C3%B1a-512528472169778"><i class="fab fa-fw fa-facebook-f"></i></a>
+                <a class="btn btn-outline-light btn-social mx-1" href="https://twitter.com/Dpositivaes"><i class="fab fa-fw fa-twitter"></i></a>
+                <a class="btn btn-outline-light btn-social mx-1" href="https://www.instagram.com/disciplinapositivaespana/"><i class="fab fa-fw fa-instagram"></i></a>
             </div>
             <!-- Footer Location-->
             <div class="col-lg-4 mb-5 mb-lg-0">
@@ -20,8 +19,8 @@
             <div class="col-lg-4">
                 <h4 class="text-uppercase mb-4">Página recomendada</h4>
                 <p class="lead mb-0">
-                    Promueve relaciones de respeto mutuo y responsabilidad en la vida.
-                    <a href="http://disciplinapositivaespana.com/">Link página oficial de España</a>
+                    Página oficial de España de positiva disciplina, para ir pincha
+                    <a href="http://disciplinapositivaespana.com/">aquí</a>
                 </p>
             </div>
         </div>

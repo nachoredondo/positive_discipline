524fe911c4dcdb2b345a958656cb1fdaae8e580e
iredondoa <iredondoa@zalcu.com>
2021-04-17 02:53:26 +0200

    style(rename table module)

--- views/table/index.php
+++ views/table/index.php
@@ -11,19 +11,19 @@ $user = User::get_user_from_user($_SESSION['user']);
 <html lang="en">
     <head>
         <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
         <meta name="description" content="" />
         <meta name="author" content="" />
         <title>Mesa</title>
         <!-- Favicon-->
-        <link rel="icon" type="image/x-icon" href="../../assets/img/favicon.ico" />
+        <link rel="icon" type="image/x-icon" href="../../assets/img/favicon.ico"/>
         <!-- Font Awesome icons (free version)-->
         <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
         <!-- Google fonts-->
         <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
         <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
         <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="../../css/styles.css" rel="stylesheet" />
+        <link href="../../css/styles.css" rel="stylesheet"/>
 
         <!-- Bootstrap core JS-->
         <script src="<?php echo APP_ROOT ?>/assets/jquery/jquery.min.js"></script>
@@ -248,7 +248,7 @@ $user = User::get_user_from_user($_SESSION['user']);
             <div class="container mb-5">
                 <!-- Rules Section Heading-->
                 <div class="mr-5">
-                    <h2 class="text-center text-uppercase text-secondary mt-4">Mesa de la calma</h2>
+                    <h2 class="text-center text-uppercase text-secondary mt-4">Mesa de la paz</h2>
                     <!-- Icon Divider-->
                     <div class="divider-custom">
                         <div class="divider-custom-line"></div>

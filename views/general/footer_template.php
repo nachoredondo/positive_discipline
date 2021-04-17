ed8226c732f340e7ff12872fd9b3a4cf11d97572
iredondoa <iredondoa@zalcu.com>
2021-04-17 02:52:42 +0200

    style(footer restructured): university icon is uploaded

--- views/general/footer_template.php
+++ views/general/footer_template.php
@@ -1,25 +1,28 @@
 <footer class="footer text-center">
     <div class="container">
         <div class="row">
-            <!-- Footer Social Icons-->
-            <div class="col-lg-4 mb-5 mb-lg-0">
-                <h4 class="text-uppercase mb-4">Redes sociales</h4>
-                <a class="btn btn-outline-light btn-social mx-1" href="https://www.facebook.com/Disciplina-Positiva-Espa%C3%B1a-512528472169778"><i class="fab fa-fw fa-facebook-f"></i></a>
-                <a class="btn btn-outline-light btn-social mx-1" href="https://twitter.com/Dpositivaes"><i class="fab fa-fw fa-twitter"></i></a>
-                <a class="btn btn-outline-light btn-social mx-1" href="https://www.instagram.com/disciplinapositivaespana/"><i class="fab fa-fw fa-instagram"></i></a>
-            </div>
             <!-- Footer Location-->
             <div class="col-lg-4 mb-5 mb-lg-0">
                 <h4 class="text-uppercase mb-4">Autor</h4>
                 <p class="lead mb-0">
                     Ignacio Redondo Arroyo
                 </p>
+                <a href="https://www.urjc.es" target="_blank">
+                    <img class="mt-2" src="../../assets/img/urjc.png" width="60">
+                </a>
+            </div>
+            <!-- Footer Social Icons-->
+            <div class="col-lg-4 mb-5 mb-lg-0">
+                <h4 class="text-uppercase mb-4">Redes sociales recomendadas</h4>
+                <a class="btn btn-outline-light btn-social mx-1" href="https://www.facebook.com/Disciplina-Positiva-Espa%C3%B1a-512528472169778"><i class="fab fa-fw fa-facebook-f"></i></a>
+                <a class="btn btn-outline-light btn-social mx-1" href="https://twitter.com/Dpositivaes"><i class="fab fa-fw fa-twitter"></i></a>
+                <a class="btn btn-outline-light btn-social mx-1" href="https://www.instagram.com/disciplinapositivaespana/"><i class="fab fa-fw fa-instagram"></i></a>
             </div>
             <!-- Footer About Text-->
             <div class="col-lg-4">
                 <h4 class="text-uppercase mb-4">Página recomendada</h4>
                 <p class="lead mb-0">
-                    Página oficial de España de positiva disciplina, para ir pincha
+                    Página oficial de España de disciplina positiva, para ir pincha
                     <a href="http://disciplinapositivaespana.com/">aquí</a>
                 </p>
             </div>

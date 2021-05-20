8308ce7ae08a9691c290efc2d9f8f76854a4e6ec
iredondoa <iredondoa@zalcu.com>
2021-05-20 07:48:49 +0200

    feat(footer year updated)

--- views/general/footer.php
+++ views/general/footer.php
@@ -1,7 +1,7 @@
 <div class="container-fluid mt-4">
 	&copy;
 	<script>
-		document.write(2020)
+		document.write(2021)
 	</script>, Developed by Ignacio Redondo Arroyo
 	<a href="https://www.urjc.es" target="_blank">
 		<img src="../../assets/img/urjc.png" width="40">

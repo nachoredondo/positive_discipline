2d27ef4034525ff788ad6465f15f8dfe29f3603f
i.redondo.arroyo <i.redondo.arroyo@gmail.com>
2022-04-26 01:36:40 +0200

    feat(year footer is updated)

--- views/general/footer.php
+++ views/general/footer.php
@@ -1,8 +1,9 @@
 <div class="container-fluid mt-4">
-	&copy;
-	<script>
-		document.write(2021)
-	</script>, Developed by Ignacio Redondo Arroyo
+	&copy; 
+	<?php
+		echo date("Y");
+	?>
+	Developed by Ignacio Redondo Arroyo
 	<a href="https://www.urjc.es" target="_blank">
 		<img src="../../assets/img/urjc.png" width="40">
 	</a>

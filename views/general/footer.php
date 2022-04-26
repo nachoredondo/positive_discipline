fee01fa7e0656b872038374c3c9bdfdd3291a3bb
i.redondo.arroyo <i.redondo.arroyo@gmail.com>
2022-04-26 19:27:56 +0200

    style(footer): text is updated

--- views/general/footer.php
+++ views/general/footer.php
@@ -3,7 +3,7 @@
 	<?php
 		echo date("Y");
 	?>
-	Developed by Ignacio Redondo Arroyo
+	, Developed by Ignacio Redondo Arroyo
 	<a href="https://www.urjc.es" target="_blank">
 		<img src="../../assets/img/urjc.png" width="40">
 	</a>
